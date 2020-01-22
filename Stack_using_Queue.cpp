537538566f42441221497033e0bb16c0d0713b34
prashant6 <prashantsingh41195@gmail.com>
2020-01-23 00:17:46 +0530

    Implemented Stack using Queues (push costly)

--- Stack_using_Queue.cpp
+++ Stack_using_Queue.cpp
@@ -7,37 +7,39 @@ class qu1
 {
     qu1 * n1;
     int val1;
-    
+
     public:
-    int enq1();
+    void enq1(int);
     int deq1();
-}*f1=NULL, *r1=NULL;
+}*f1=NULL, *r1=NULL, *q1=NULL;
 
 
-int qu1::enq1(int a)
+void qu1::enq1(int a)
 {
     qu1 *pt1;
     pt1 = new qu1;
     pt1->n1=NULL;
-    
-    if(n1==NULL)
+
+    if(pt1==NULL)
         cout<<"OVERFLOW !!";
-    
+
     else if(f1==NULL)
     {
         f1 = pt1;
         r1 = pt1;
+        q1 = pt1;
+
         pt1->val1=a;
     }
-    
-    else 
+
+    else
     {
         r1->n1=pt1;
         r1=pt1;
         pt1->val1=a;
-        
+
     }
-    
+
 }
 
 
@@ -46,66 +48,69 @@ int qu1::deq1()
     int a;
     qu1 * t1;
     t1=f1;
-    
+
     if(t1==NULL)
         cout<<"UNDERFLOW !!";
-    
+
     else if(f1==r1)
     {
         a = t1->val1;
         f1=NULL;
         r1=NULL;
-        
+        q1=NULL;
+
         delete t1;
         return a;
     }
-    
+
     else
     {
         a = t1->val1;
         f1=f1->n1;
-        
+
         delete t1;
         return a;
     }
-       
+
 }
 
 
 class qu2
 {
     qu2 *n2;
-    int pt2;
-    
+    int val2;
+
     public:
-    int enq2();
+    void enq2(int);
     int deq2();
-}*f2=NULL, *r2=NULL;
+}*f2=NULL, *r2=NULL, *q2=NULL;
 
 
-int qu2::enq2(int a)
+void qu2::enq2(int a)
 {
     qu2 *pt2;
     pt2 = new qu2;
     pt2->n2=NULL;
-    
-    if(n2==NULL)
+
+    if(pt2==NULL)
         cout<<"OVERFLOW !!";
-    
+
     else if(f2==NULL)
     {
         f2 = pt2;
         r2 = pt2;
+        q2 = pt2;
+
         pt2->val2=a;
     }
-    
-    else 
+
+    else
     {
         r2->n2=pt2;
         r2=pt2;
         pt2->val2=a;
     }
-    return a;   
+
 }
 
 
@@ -114,35 +119,75 @@ int qu2::deq2()
     int a;
     qu2 * t2;
     t2=f2;
-    
+
     if(t2==NULL)
         cout<<"UNDERFLOW !!";
-    
+
     else if(f2==r2)
     {
         a = t2->val2;
         f2=NULL;
         r2=NULL;
-        
+        q2=NULL;
+
         delete t2;
         return a;
     }
-    
+
     else
     {
         a = t2->val2;
         f2=f2->n2;
-        
+
         delete t2;
         return a;
     }
-       
+
 }
 
 int main()
 {
-    
-    
+    int val, nos, garbage;
+    qu1 obj1;
+    qu1 *temp=f1;
+    qu2 obj2;
+
+    cout<<"Enter no of values to be pushed :";
+    cin>>nos;
+
+    while(nos!=0)
+    {
+        cout<<"Enter val :";
+        cin>>val;
+
+        if(q1==NULL)
+            obj1.enq1(val);
+
+        else
+        {
+            while(q1!=NULL)
+            {
+                obj2.enq2(obj1.deq1());
+            }
+
+            obj1.enq1(val);
+
+            while(q2!=NULL)
+            {
+                obj1.enq1(obj2.deq2());
+            }
+        }
+
+        nos--;
+    }
+
+    while(q1!=NULL)
+    {
+        garbage = obj1.deq1();
+        cout<<"values in stack :"<<garbage<<"\n";
+
+    }
+
     return 0;
 }
 
